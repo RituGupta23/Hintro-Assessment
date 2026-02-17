43935284568e143c7fc0993bd8bca9777e938e1a
Ritu Gupta <ritugupta@Ritus-MacBook-Pro.local>
2026-02-17 13:30:55 +0530

    Changed placeholder

--- frontend/src/pages/SignupPage.tsx
+++ frontend/src/pages/SignupPage.tsx
@@ -43,7 +43,7 @@ export const SignupPage: React.FC = () => {
                             <input
                                 id="name"
                                 type="text"
-                                placeholder="John Doe"
+                                placeholder="your name"
                                 value={name}
                                 onChange={(e) => setName(e.target.value)}
                                 required
